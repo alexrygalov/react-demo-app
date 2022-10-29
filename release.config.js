805f32e764942ed36d8ed497860869c3c1ccf412
Aleksandr Rygalov <14346955+alexrygalov@users.noreply.github.com>
2022-10-29 20:46:48 +0200

    Add semantic release plugin (#6)
    * Initialize project using Create React App
    
    * First commit
    
    * Add basic workflow
    
    * Move to workflow folder
    
    * Ignore .idea folder
    
    * Fix prettier warnings
    
    * Add build project on push flow
    
    * Test changes (#3)
    
    * Test some changes on PR
    
    * Trigger ci
    
    * Revert "Trigger ci"
    
    This reverts commit 6786bcbd7a181029cb9401a7b009fd8efb1475fd.
    
    * Add caching (#4)
    
    * Cleanup
    
    * Add caching
    
    * Fix restore-keys param
    
    * Upload artifacts
    
    * fix: test semantic-release plugin
    
    * feat: add release to the main branch
    
    * fix: fix step name
    
    * fix: run prettier

--- release.config.js
+++ release.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-    branches: "main",
-    repositoryUrl: "https://github.com/alexrygalov/react-demo-app",
-    plugins: ["@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/npm",
-        "@semantic-release/github"]
-}
+  branches: "main",
+  repositoryUrl: "https://github.com/alexrygalov/react-demo-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+  ],
+};
