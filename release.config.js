c31fd54b830e655a6f7da7df561ee33fb0b187fa
Aleksandr Rygalov <14346955+alexrygalov@users.noreply.github.com>
2022-10-29 20:33:36 +0200

    fix: test semantic-release plugin

---
+++ release.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    branches: "main",
+    repositoryUrl: "https://github.com/alexrygalov/react-demo-app",
+    plugins: ["@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/npm",
+        "@semantic-release/github"]
+}
