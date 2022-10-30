f644755e4825b392642fef46427d3c62ad6f12bf
Aleksandr Rygalov <14346955+alexrygalov@users.noreply.github.com>
2022-10-30 07:34:55 +0100

    ci: create issue of failure test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Pew Pew
+          Learn React Pew
         </a>
       </header>
     </div>
