832dfa7ba6705ae2a1f15ae3abf95ec32409fb40
Aleksandr Rygalov <14346955+alexrygalov@users.noreply.github.com>
2022-10-30 07:42:12 +0100

    ci: revert broken test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Pew
+          Learn React Pew Pew
         </a>
       </header>
     </div>
