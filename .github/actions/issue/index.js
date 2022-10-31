7737c3df37988737590d3e3a7f28d2dcf64a9b58
Aleksandr Rygalov <14346955+alexrygalov@users.noreply.github.com>
2022-10-31 21:33:13 +0100

    ci: fix auth key

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -1,6 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const { Octokit } = require("./dist");
+const {Octokit} = require("octokit");
 
 try {
   const token = core.getInput("token");
